f843a3f0321d5f092d2d4ebc5d4a258624e92e51
marshal2327 <marshalaugustine23@gmail.com>
2026-02-28 12:58:06 +0530

    update

--- assets/js/emp_entry.js
+++ assets/js/emp_entry.js
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
         return new Promise(resolve =>{
             canvas.toBlob(blob => {
                 resolve(blob);
-            },'image/jpeg', 0.4);
+            },'image/jpeg', 0.3);
         });
         
     }
