5e5ee5240d098d5eb54eaa2f89990a541d0daf6a
marshal2327 <marshalaugustine23@gmail.com>
2026-03-04 19:56:15 +0530

    updare

--- application/views/pages/emp_entry.php
+++ application/views/pages/emp_entry.php
@@ -98,9 +98,9 @@ defined('BASEPATH') or exit('NO DIRECT SCRIPT ACCESS ALLOWED');
                     <!-- </div> -->
                     <div class="card-title d-flex justify-content-evenly pt-3">
 
-                        <div style='width:115px; height:140px;' class="card rounded-3 shadow">
+                        <div style='width:110px; height:125px;' class="card rounded-3 shadow">
                             <img id='emp_img' class="card-img-top rounded-top-3 "
-                                style='width:100%; min-height:115px; border-bottom:1px solid grey;'
+                                style='width:100%; min-height:100px; border-bottom:1px solid grey;'
                                 src="<?php echo base_url()?>assets/images/nouserimg.jpg" alt="user_prof_pic">
                             <div
                                 class="card-body p-0 bg-light rounded-bottom  h-100 d-flex justify-content-center align-items-center">
@@ -109,7 +109,7 @@ defined('BASEPATH') or exit('NO DIRECT SCRIPT ACCESS ALLOWED');
                             </div>
                         </div>
 
-                        <div id='cap_img_box' style='width:115px; height:140px; display:none;'
+                        <div id='cap_img_box' style='width:110px; height:125px; display:none;'
                             class="card rounded-3 shadow">
                             <img id='cap_img' class="card-img-top rounded-top-3"
                                 style='width:100%; min-height:100px; border-bottom:1px solid grey;'
