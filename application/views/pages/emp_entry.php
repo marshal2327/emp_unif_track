0b2499a94127abbfccddba6a5d50dd4ccf00fc52
marshal2327 <marshalaugustine23@gmail.com>
2026-03-05 09:44:29 +0530

    update

--- application/views/pages/emp_entry.php
+++ application/views/pages/emp_entry.php
@@ -9,7 +9,7 @@ defined('BASEPATH') or exit('NO DIRECT SCRIPT ACCESS ALLOWED');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EMP UNIFORM ENTRY</title>
-    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/emp_entry.css?v=254">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/emp_entry.css?v=369">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
